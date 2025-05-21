a3d83bcb0c9f53b5b7383538bab327357eba7736
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-05-21 08:01:07 +0600

    use loader while navigating routes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import Spinner from "../components/ui/Spinner";
 
 const MainLayout = () => {
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
+
   return (
     <div>
       <Header />
-      <div className="min-h-[calc(100vh-378px)]">
-        <Outlet />
-      </div>
+      {isNavigating ? (
+        <Spinner />
+      ) : (
+        <div className="min-h-[calc(100vh-378px)]">
+          <Outlet />
+        </div>
+      )}
       <Footer />
     </div>
   );
