9d586d5c52597bff21e853d74e7ebc167d88de58
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-06-26 19:08:35 +0600

    remove unnecessary package

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import Banner from "../components/Banner/Banner";
 import FeaturedGroups from "../components/FeaturedGroups/FeaturedGroups";
 import Testimonials from "../components/Testimonials/Testimonials";
 import WhyToChoose from "../components/WhyToChoose/WhyToChoose";
 import { useLoaderData } from "react-router";
-import "./Home/Banner/embla.css";
-import EmblaCarousel from "./Home/Banner/EmblaCarousel";
 import HeroSlider from "./Home/Banner/HeroSlider";
 import OurMission from "./AboutUs/OurMission";
-
-const OPTIONS = {};
-const SLIDE_COUNT = 5;
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 const Home = () => {
   const groups = useLoaderData();
